867364247d621131f9c4968d754086864bb95295
kleon6436 <knreon.rlwy.pho@gmail.com>
2020-09-24 23:20:50 +0900

    Removed unnecessary using code.

--- PhotoViewer/ViewModels/ImageEditToolViewModel.cs
+++ PhotoViewer/ViewModels/ImageEditToolViewModel.cs
@@ -7,7 +7,6 @@ using System.Collections.ObjectModel;
 using System.IO;
 using System.Windows;
 using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
 
 namespace Kchary.PhotoViewer.ViewModels
