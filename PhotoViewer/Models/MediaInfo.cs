90267239781ba58fe5fa02bb4dbd1e34f6f9aa73
kleon6436 <knleon.801.jpt@outlook.jp>
2022-10-08 23:46:10 +0900

    Removed unused using statement.

--- PhotoViewer/Models/MediaInfo.cs
+++ PhotoViewer/Models/MediaInfo.cs
@@ -1,7 +1,6 @@
 ﻿using Kchary.PhotoViewer.Helpers;
 using System;
 using System.Collections.Generic;
-using System.Collections.Specialized;
 using System.IO;
 using System.Linq;
 using System.Windows.Media;
